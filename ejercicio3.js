6fab3835c4ae77754665aef5b35546e12d74536d
luciaacordoba17 <luciaacordoba17@gmail.com>
2026-04-15 01:59:10 -0300

    Add files via upload

---
+++ ejercicio3.js
@@ -0,0 +1,8 @@
+let usuario = "admin";
+let contraseña = "1234";
+
+if (usuario == "admin" & contraseña == "1234") {
+    console.log("Ingreso permitido");
+} else {
+    console.log("Ingreso rechazado");
+}
